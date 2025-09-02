4f7f950b551cd47204cff5cac2593ff7f97cf64d
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2025-09-02 18:36:34 -0300

    Se crean los archivos de los repositorios

---
+++ grpc-server/src/main/java/com/grpc/grpc_server/repositories/DonationsAtEventsRepository.java
@@ -0,0 +1,13 @@
+package com.grpc.grpc_server.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.grpc.grpc_server.entities.DonationsAtEvents;
+public class DonationsAtEventsRepository {
+    
+}
