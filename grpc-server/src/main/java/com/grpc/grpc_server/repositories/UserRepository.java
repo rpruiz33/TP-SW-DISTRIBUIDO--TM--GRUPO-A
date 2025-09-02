c37d576691251673a828b8364a6ed773a8dadb1c
Roberto Andres Ruiz Pereira <rpruiz33@gmail.com>
2025-09-02 16:31:27 -0300

    funcionaidad de registro de usuario

--- grpc-server/src/main/java/com/grpc/grpc_server/repositories/UserRepository.java
+++ grpc-server/src/main/java/com/grpc/grpc_server/repositories/UserRepository.java
@@ -1,16 +1,21 @@
 package com.grpc.grpc_server.repositories;
 
-import com.grpc.grpc_server.entities.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.grpc.grpc_server.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE (u.email = :email and u.password = :password) or (u.username = :username and u.password = :password) ")
-    Optional <User> findByEmailOrUsername(@Param("email") String email, @Param("username") String username, @Param("password") String password );
+    @Query("SELECT u FROM User u WHERE (u.email = :email and u.password = :password) or (u.username = :username and u.password = :password)")
+    Optional<User> findByEmailOrUsername(@Param("email") String email, @Param("username") String username, @Param("password") String password);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
--- grpc-server/src/main/java/com/grpc/grpc_server/serviceimp/UserService.java
+++ grpc-server/src/main/java/com/grpc/grpc_server/serviceimp/UserService.java
@@ -1,17 +1,17 @@
 package com.grpc.grpc_server.serviceimp;
 
-import com.grpc.grpc_server.entities.User;
-import com.grpc.grpc_server.repositories.UserRepository;
-import io.grpc.stub.StreamObserver;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.grpc.grpc_server.MyServiceClass;
-import com.grpc.grpc_server.MyServiceClass.LoginRequest;
-import com.grpc.grpc_server.MyServiceClass.LoginResponse;
-import com.grpc.grpc_server.MyServiceGrpc;
 import org.springframework.grpc.server.service.GrpcService;
 
-import java.util.Optional;
+import com.grpc.grpc_server.MyServiceClass.LoginRequest;
+import com.grpc.grpc_server.MyServiceClass.LoginResponse;
+import com.grpc.grpc_server.MyServiceClass.RegisterRequest;
+import com.grpc.grpc_server.MyServiceClass.RegisterResponse;
+import com.grpc.grpc_server.MyServiceGrpc;
+import com.grpc.grpc_server.entities.User;
+import com.grpc.grpc_server.repositories.UserRepository;
+
+import io.grpc.stub.StreamObserver;
 
 @GrpcService
 public class UserService extends MyServiceGrpc.MyServiceImplBase {
@@ -19,22 +19,49 @@ public class UserService extends MyServiceGrpc.MyServiceImplBase {
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
         var responseBuilder = LoginResponse.newBuilder();
 
-        userRepository.findByEmailOrUsername(request.getUsername(), request.getUsername(), request.getPassword())            .ifPresentOrElse(user -> {
-            if (user.getPassword().equals(request.getPassword())) {
-                responseBuilder.setSuccess(true).setMessage("Login successful");
-            } else {
-                responseBuilder.setSuccess(false).setMessage("Invalid password");
-            }
-        }, () -> {
-            responseBuilder.setSuccess(false).setMessage("User not found");
-        });
+        userRepository.findByEmailOrUsername(request.getUsername(), request.getUsername(), request.getPassword())
+            .ifPresentOrElse(user -> {
+                if (user.getPassword().equals(request.getPassword())) {
+                    responseBuilder.setSuccess(true).setMessage("Login successful");
+                } else {
+                    responseBuilder.setSuccess(false).setMessage("Invalid password");
+                }
+            }, () -> {
+                responseBuilder.setSuccess(false).setMessage("User not found");
+            });
 
         responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
     }
+
+    @Override
+public void register(RegisterRequest request, StreamObserver<RegisterResponse> responseObserver) {
+    var responseBuilder = RegisterResponse.newBuilder();
+
+    if (userRepository.existsByEmail(request.getEmail()) || userRepository.existsByUsername(request.getUsername())) {
+        responseBuilder.setSuccess(false).setMessage("User already exists");
+    } else {
+       
+        if (request.getEmail().isEmpty() || request.getUsername().isEmpty() || request.getPassword().isEmpty()) {
+            responseBuilder.setSuccess(false).setMessage("Email, username, and password are required");
+        } else {
+        
+            User newUser = new User();
+            newUser.setUsername(request.getUsername());
+            newUser.setEmail(request.getEmail());
+          
+            newUser.setPassword(request.getPassword());
+
+            userRepository.save(newUser);
+            responseBuilder.setSuccess(true).setMessage("User registered successfully");
+        }
+    }
+
+    responseObserver.onNext(responseBuilder.build());
+    responseObserver.onCompleted();
+}
 }
