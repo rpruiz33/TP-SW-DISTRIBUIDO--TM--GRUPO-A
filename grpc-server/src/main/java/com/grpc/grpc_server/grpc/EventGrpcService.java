2711c7e20ef211d65abbab187c53485f8eaeb23c
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2025-09-05 14:20:17 -0300

    Agrego DTO para la clase Event

--- grpc-server/src/main/java/com/grpc/grpc_server/grpc/EventGrpcService.java
+++ grpc-server/src/main/java/com/grpc/grpc_server/grpc/EventGrpcService.java
@@ -7,32 +7,40 @@ import com.grpc.grpc_server.mapper.EventMapper;
 import com.grpc.grpc_server.services.EventService;
 
 import io.grpc.stub.StreamObserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.grpc.server.service.GrpcService;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @GrpcService
 public class EventGrpcService extends EventServiceGrpc.EventServiceImplBase {
 
-    private final EventService eventService;
 
-    public EventGrpcService(EventService eventService) {
-        this.eventService = eventService;
-    }
+
+        @Autowired
+        private EventService eventService;
 
     @Override
     public void getAllEvents(MyServiceClass.Empty request, StreamObserver<MyServiceClass.EventListResponse> responseObserver) {
 
+         // 1️⃣ Obtener entidades desde la capa service
         List<Event> events = eventService.getAllEvents();
 
-        MyServiceClass.EventListResponse.Builder responseBuilder = MyServiceClass.EventListResponse.newBuilder();
+        // 2️⃣ Mapear a Proto usando Mapper
+        List<MyServiceClass.Event> grpcEvents = events.stream()
+                .map(EventMapper::toProto)
+                .collect(Collectors.toList());
 
-        for (Event e : events) {
-            responseBuilder.addEvents(EventMapper.toProto(e));
-        }
+        // 3️⃣ Construir y enviar la respuesta
+        MyServiceClass.EventListResponse response = MyServiceClass.EventListResponse.newBuilder()
+                .addAllEvents(grpcEvents)
+                .build();
 
-        responseObserver.onNext(responseBuilder.build());
+
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
