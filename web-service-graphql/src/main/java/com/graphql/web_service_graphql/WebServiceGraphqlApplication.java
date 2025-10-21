40f6c67b4a112670354e5a2c542227a6f3dbe327
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2025-10-21 12:15:22 -0300

    Inicialización web-service-graphql

---
+++ web-service-graphql/src/main/java/com/graphql/web_service_graphql/WebServiceGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.web_service_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceGraphqlApplication.class, args);
+	}
+
+}
